9458f2a3586a7ed12702b95eb0e4696374965114
Renan de Freitas <rnn.frts@gmail.com>
2021-07-28 14:52:22 -0300

    updated footer colors and link structure

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import { Container } from './styles'
 import { Link } from 'react-router-dom'
+import githubIcon from '../../images/github.png'
+import linkedinIcon from '../../images/linkedin.png'
+import mailIcon from '../../images/mail.png'
+import logo from '../../images/logo-renan.png'
+
 
 const Footer = () => {
   return (
     <Container>
       <div>
-        <Link to="/">Voltar</Link>
-        <Link to="/">Blog</Link>
-        <a href="https://defreitas.xyz/">Projetos</a>
+        <Link to="/">Back</Link>
+        <a href="https://github.com/renansdf" target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="Logo do github" /></a>
+        <a href="https://www.linkedin.com/in/renan-sdf" target="_blank" rel="noopener noreferrer"><img src={linkedinIcon} alt="Logo do LinkedIn" /></a>
+        <a href="mailto:renan@defreitas.xyz" target="_blank" rel="noopener noreferrer"><img src={mailIcon} alt="Ícone de email" /></a>
       </div>
 
-
-      <a href="https://defreitas.xyz/"><img src="../images/logo-branca.png" alt="Logo renan" /></a>
-
-
-
+      <a href="https://defreitas.xyz/"><img src={logo} alt="Logo renan" /></a>
     </Container>
   )
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,13 +2,13 @@ import styled from 'styled-components'
 
 export const Container = styled.nav`
   width: 90%;
-  max-width: 850px;
+  max-width: 570px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 50px 0px;
   margin: 50px 0;
-  border-top: 1px solid #fff4eb;
+  border-top: 1px solid #ff0025;
   
   > a:first-child{
     width: 150px;
@@ -29,17 +29,29 @@ export const Container = styled.nav`
   }
 
   div a:first-child{
-    background-color: #fff4eb;
+    background-color: transparent;
+    color: #202020;
     color: rgba(24,71,43,1);
-    padding: 10px 20px;
+    padding: 5px 13px;
     font-size: 13px;
     text-transform: uppercase;
+    border: 1px solid #202020;
 
     transition: all .4s;
 
     &:hover{
-      background-color:#ff7300;
-      color: #fff4eb;
+      background-color:#202020;
+      color: #fff;
     }
+  }
+
+  div{
+    display: flex;
+    align-items: center;
+  }
+
+  a img{
+    max-width: 30px;
+    max-height: 30px;
   }
 `;
